79c33a95659cf63a165b2116e05ea8641e6389a7
alex10017 <alex.bradshaw1@outlook.com>
2022-09-11 22:12:06 +0100

    Collasping Navbar

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-    document.getElementById("navbar").style.boxShadow = "0px -12px 20px 20px #242323";
+    document.getElementById("navbar").style.boxShadow = "0px -12px 20px 20px rgb(36, 35, 35)";
   } else {
     document.getElementById("navbar").style.top = "-80px";
-    document.getElementById("navbar").style.boxShadow = "0px -12px 20px 20px #202020";
+    document.getElementById("navbar").style.boxShadow = "0px -12px 20px 20px #1d1c1c";
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+
+/* Animation only when page is loaded */
+/*
+window.addEventListener("load", () => {
+  document.querySelector("body").classList.add("loaded");
+})
+*/
